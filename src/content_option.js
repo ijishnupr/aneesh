409ac69a446e7acc811c89c59706e289cf36a5f1
jishnu <achu619754@gmail.com>
2025-02-28 18:23:17 +0530

    Added new content

--- src/content_option.js
+++ src/content_option.js
@@ -7,7 +7,7 @@ const meta = {
 const introdata = {
     title: "Hi, I'm Aneesh",
     animated: {
-        first: "Roto Artist",
+        first: "Vfx Artist",
         second: "Vfx compositor",
         third: "Animation",
         fourth: "Motion blur",
@@ -21,7 +21,7 @@ const introdata = {
 
 const dataabout = {
     title: "Who am I?",
-    aboutme: "I am Aneesh, a dedicated and experienced VFX professional with a passion for bringing imagination to life through visual effects. My journey in the world of VFX has been driven by a relentless pursuit of creativity, technical excellence, and a commitment to delivering exceptional visual storytelling .My career has been shaped by years of honing my skills in various aspects of visual effects, including compositing, rotoscoping, 3D modeling, animation, and dynamic simulations.I ve had the privilege of working on a diverse range of projects, from feature films to advertising campaigns, each presenting unique challenges and opportunities to push the boundaries of what is visually possible.What sets me apart is not only my technical proficiency but also my ability to work collaboratively within a team.",
+    aboutme: "I am Aneesh, VFX Prep and Paint Artist As a skilled VFX Prep and Paint Artist with a keen eye for detail, I specialize in preparing and enhancing visual elements for seamless integration into high quality visual effects. With expertise in digital matte painting, roto, tracking, and clean-up, I ensure the highest standards of visual fidelity. My experience spans working with industry-standard software such as Nuke, Silhouette, Photoshop, and Mocha",
 };
 const worktimeline = [{
     jobtitle: "Example 1",
